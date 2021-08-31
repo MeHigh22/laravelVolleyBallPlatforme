77b1e1d6a4964444c02e84b0e75ba5374de56413
MeHigh22 <72247500+MeHigh22@users.noreply.github.com>
2021-08-31 15:07:27 +0200

    Edit et Show pour Equipe

--- resources/views/admin/teams/teamsAll.blade.php
+++ resources/views/admin/teams/teamsAll.blade.php
@@ -7,20 +7,22 @@
   <div class="relative grid gap-5 sm:grid-cols-2 lg:grid-cols-4">
     @foreach ($teams as $team)
     <div class="flex flex-col justify-between overflow-hidden text-left transition-shadow duration-200 bg-white rounded shadow-xl group hover:shadow-2xl">
-      <div class="p-5">
-        <div class="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
-          <svg class="w-8 h-8 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
-            <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
-          </svg>
-        </div>
-        <p class="mb-2 font-bold">{{$team->club}}</p>
-        <p class="text-sm leading-5 text-gray-900">
-         City :  {{$team->city}}
-        </p>
-        <p class="text-sm leading-5 text-gray-900">
-         Max Players : {{$team->maxplayers}}
-        </p>
-      </div>
+        <a href={{"team/" . $team->id}}>
+            <div class="p-5">
+            <div class="flex items-center justify-center w-10 h-10 mb-4 rounded-full bg-indigo-50">
+              <svg class="w-8 h-8 text-deep-purple-accent-400" stroke="currentColor" viewBox="0 0 52 52">
+                <polygon stroke-width="3" stroke-linecap="round" stroke-linejoin="round" fill="none" points="29 13 14 29 25 29 23 39 38 23 27 23"></polygon>
+              </svg>
+            </div>
+            <p class="mb-2 font-bold">{{$team->club}}</p>
+            <p class="text-sm leading-5 text-gray-900">
+             City :  {{$team->city}}
+            </p>
+            <p class="text-sm leading-5 text-gray-900">
+             Max Players : {{$team->maxplayers}}
+            </p>
+          </div>
+        </a>
       <div class="w-full h-1 ml-auto duration-300 origin-left transform scale-x-0 bg-deep-purple-accent-400 group-hover:scale-x-100"></div>
     </div>
     @endforeach
--- resources/views/admin/teams/teamsShow.blade.php
+++ resources/views/admin/teams/teamsShow.blade.php
@@ -1,1 +1,52 @@
 @extends("layouts.indexBack")
+
+@section("content")
+    <section class="bg-white dark:bg-gray-800">
+        <div class="container px-6 py-8 mx-auto">
+            <div class="w-full mt-8 lg:w-1/2">
+                <div class="lg:max-w-lg">
+                    <h1 class="text-2xl font-medium tracking-wide text-gray-800 dark:text-white lg:text-4xl">{{$team->club}}</h1>
+                    <p class="mt-2 text-gray-600 dark:text-gray-300">Details Club : </p>
+
+                    <div class="grid gap-6 mt-8 sm:grid-cols-2">
+                        <div class="block text-gray-800 dark:text-gray-200">
+
+                            <h2 class="text-2xl font-medium tracking">Name </h2>
+                            <h2>{{$team->club}}</h2>
+                        </div>
+
+                        <div class="items-center block text-gray-800 dark:text-gray-200">
+
+
+                            <h2 class="text-2xl font-medium tracking">City</h2>
+                            <h2>{{$team->city}}</h2>
+                        </div>
+
+                        <div class="items-center block text-gray-800 dark:text-gray-200">
+
+                            <h2 class="text-2xl font-medium tracking">Country</h2>
+                            <h2>{{$team->country}}</h2>
+                        </div>
+
+                        <div class="items-center block text-gray-800 dark:text-gray-200">
+                            <h2 class="text-2xl font-medium tracking">Continent</h2>
+                            <h2>{{$team->continents->continent}}</h2>
+                        </div>
+
+                        <form action={{route("team.destroy", $team->id)}} method="POST" class="items-center block text-gray-800 dark:text-gray-200">
+                            @csrf
+                            @method("DELETE")
+                            <button type ="submit" class="btn btn-outline btn-secondary">Delete</button>
+                        </form>
+
+                        <div>
+                            <a href="{{route("team.edit",$team->id)}}"><button class="btn btn-outline btn-accent">Edit</button></a>
+                        </div>
+
+                    </div>
+                </div>
+        </div>
+            </div>
+    </section>
+
+@endsection
